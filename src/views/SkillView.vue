0b00c503948d7b3c9f6683a04a862488e952d740
TaufikDev <onlytaufik47@gmail.com>
2023-04-25 20:28:26 +0700

    Halaman Skill 2 Selesai

--- src/views/SkillView.vue
+++ src/views/SkillView.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="container">
-        <div class="cards">
+        <div :class="slideNext" @click="changeSlide()"><h2>→</h2></div>
+        <div :class="slideNone" @click="changeSlide2()"><h2>←</h2></div>
+        <div :class="slide1">
             <div class="circleutama">
                 <div class="circle">
                     <img :src="html" alt="logo html">
@@ -9,7 +11,7 @@
             <h2>HTML</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos dolores ea adipisci assumenda maiores consequuntur dignissimos hic itaque consequatur expedita.</p>
         </div>
-        <div class="cards">
+        <div :class="slide1">
             <div class="circleutama">
                 <div class="circle">
                     <img :src="css" alt="logo css">
@@ -18,7 +20,7 @@
             <h2>CSS</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos dolores ea adipisci assumenda maiores consequuntur dignissimos hic itaque consequatur expedita.</p>
         </div>
-        <div class="cards">
+        <div :class="slide1">
             <div class="circleutama">
                 <div class="circle">
                     <img :src="js" alt="logo javascript">
@@ -27,7 +29,7 @@
             <h2>JAVASCRIPT</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos dolores ea adipisci assumenda maiores consequuntur dignissimos hic itaque consequatur expedita.</p>
         </div>
-        <div class="cards">
+        <div :class="slide1">
             <div class="circleutama">
                 <div class="circle">
                     <img :src="sass" alt="logo sass">
@@ -36,7 +38,7 @@
             <h2>SASS</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos dolores ea adipisci assumenda maiores consequuntur dignissimos hic itaque consequatur expedita.</p>
         </div>
-        <div class="cards">
+        <div :class="slide1">
             <div class="circleutama">
                 <div class="circle">
                     <img :src="bootstrap" alt="logo bootstrap">
@@ -45,13 +47,67 @@
             <h2>BOOTSTRAP</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos dolores ea adipisci assumenda maiores consequuntur dignissimos hic itaque consequatur expedita.</p>
         </div>
-        <div class="cards">
+        <div :class="slide1">
             <div class="circleutama">
                 <div class="circle">
                     <img :src="vue" alt="logo vue">
                 </div>
             </div>
             <h2>VUE JS</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos dolores ea adipisci assumenda maiores consequuntur dignissimos hic itaque consequatur expedita.</p>
+        </div>
+        <div :class="slide2">
+            <div class="circleutama">
+                <div class="circle">
+                    <img :src="tailwind" alt="logo tailwind">
+                </div>
+            </div>
+            <h2>TAILWIND</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos dolores ea adipisci assumenda maiores consequuntur dignissimos hic itaque consequatur expedita.</p>
+        </div>
+        <div :class="slide2">
+            <div class="circleutama">
+                <div class="circle">
+                    <img :src="nextjs" alt="logo nextjs">
+                </div>
+            </div>
+            <h2>NEXT JS</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos dolores ea adipisci assumenda maiores consequuntur dignissimos hic itaque consequatur expedita.</p>
+        </div>
+        <div :class="slide2">
+            <div class="circleutama">
+                <div class="circle">
+                    <img :src="nodejs" alt="logo Node JS">
+                </div>
+            </div>
+            <h2>NODE JS</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos dolores ea adipisci assumenda maiores consequuntur dignissimos hic itaque consequatur expedita.</p>
+        </div>
+        <div :class="slide2">
+            <div class="circleutama">
+                <div class="circle">
+                    <img :src="github" alt="logo github">
+                </div>
+            </div>
+            <h2>GITHUB</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos dolores ea adipisci assumenda maiores consequuntur dignissimos hic itaque consequatur expedita.</p>
+        </div>
+        <div :class="slide2">
+            <div class="circleutama">
+                <div class="circle">
+                    <img :src="gitlab" alt="logo gitlab">
+                </div>
+            </div>
+            <h2>GITLAB</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos dolores ea adipisci assumenda maiores consequuntur dignissimos hic itaque consequatur expedita.</p>
+        </div>
+        <div :class="slide2">
+            <div class="circleutama">
+                <div class="circle">
+                    <img :src="figma" alt="logo figma">
+                </div>
+            </div>
+            <h2>FIGMA</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos dolores ea adipisci assumenda maiores consequuntur dignissimos hic itaque consequatur expedita.</p>
         </div>
         <div class="clear"></div>
@@ -65,6 +121,12 @@ import js1 from './../assets/javascript-39395.png';
 import sass1 from './../assets/pngegg.png';
 import bootstrap1 from './../assets/pngegg (1).png';
 import vue1 from './../assets/pngegg (2).png';
+import tailwind1 from './../assets/tailwind-css-icon.png';
+import nextjs1 from './../assets/nextjs-icon.png';
+import nodejs1 from './../assets/node-js-black-icon.png';
+import github1 from './../assets/github-icon.png';
+import gitlab1 from './../assets/gitlab-icon.png';
+import figma1 from './../assets/figma-icon.png';
 export default {
   data(){
     return{
@@ -73,7 +135,31 @@ export default {
       js: js1,
       sass: sass1,
       bootstrap: bootstrap1,
-      vue: vue1
+      vue: vue1,
+      tailwind: tailwind1,
+      nextjs: nextjs1,
+      nodejs: nodejs1,
+      github: github1,
+      gitlab: gitlab1,
+      figma: figma1,
+      slide1:"cards1",
+      slide2:"cards2",
+      slideNext:"slide",
+      slidePrev:"slideP",
+      slideNone:"slideN"
+    }
+  }, methods:{
+    changeSlide(){
+        this.slide1 ="cards2";
+        this.slide2 ="cards1";
+        this.slideNext = "slideN"
+        this.slideNone = "slideP"
+    },
+    changeSlide2(){
+        this.slide2 ="cards2";
+        this.slide1 ="cards1";
+        this.slideNone = "slideN";
+        this.slideNext = "slide";
     }
   }
 }
@@ -87,14 +173,38 @@ export default {
     margin: 30px auto;
     padding: 20px 0px;
 }
-.cards {
+.slide{
+    width: 50px;
+    background-color: #07806A;
+    position: absolute;
+    margin: -20px 0px 0px 1200px;
+    border-radius: 0px 20px 0px 0px;
+    color: white;
+    min-height: 50px;
+    cursor: pointer;
+}
+.slideP{
+    width: 50px;
+    background-color: #07806A;
+    position: absolute;
+    margin: -20px 0px 0px 0px;
+    border-radius: 20px 0px 0px 0px;
+    color: white;
+    min-height: 50px;
+    cursor: pointer;
+}
+.slideN{
+    display: none;
+}
+
+.cards1{
     text-align: left;
     width: 500px;
     display: inline-block;
-    margin-bottom: 20px;
+    margin: 0px 0px 20px 40px;
     color: white;
 }
-.cards .circleutama{
+.cards1 .circleutama{
     background-color: #72D7C4;
     margin-right: 100px;
     float: left;
@@ -102,16 +212,18 @@ export default {
     border-radius: 50%;
     height: 150px;
 }
-.cards .circleutama .circle{
+.cards1 .circleutama .circle{
     background-color: #07806A;
     width: 150px;
     height: 150px;
     border-radius: 50%;
 }
-.cards .circleutama .circle img{
-    width: 150px;
-    margin: 0px 0px 0px 0px;
+.cards1 .circleutama .circle img{
+    width: 165px;
     border-radius: 50%;
+}
+.cards2 {
+    display: none;
 }
 .clear{
     clear: both;
